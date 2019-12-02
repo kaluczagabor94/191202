097f657bb799fbbacabad247c6df31e342ed3339
Kalucza Gábor <kaluczagabor94@gmail.com>
2019-12-02 18:31:10 +0100

    Add project files.

---
+++ 191202/FrmMain.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace _191202
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dgvEpuletek = new System.Windows.Forms.DataGridView();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Telephely = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Ferohely = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEpuletek)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvEpuletek
+            // 
+            this.dgvEpuletek.AllowUserToAddRows = false;
+            this.dgvEpuletek.AllowUserToDeleteRows = false;
+            this.dgvEpuletek.AllowUserToResizeColumns = false;
+            this.dgvEpuletek.AllowUserToResizeRows = false;
+            this.dgvEpuletek.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvEpuletek.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvEpuletek.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvEpuletek.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEpuletek.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.Telephely,
+            this.Ferohely});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvEpuletek.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvEpuletek.Location = new System.Drawing.Point(12, 13);
+            this.dgvEpuletek.Name = "dgvEpuletek";
+            this.dgvEpuletek.RowHeadersVisible = false;
+            this.dgvEpuletek.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvEpuletek.Size = new System.Drawing.Size(776, 248);
+            this.dgvEpuletek.TabIndex = 0;
+            // 
+            // Id
+            // 
+            this.Id.FillWeight = 1F;
+            this.Id.HeaderText = "Id";
+            this.Id.Name = "Id";
+            // 
+            // Telephely
+            // 
+            this.Telephely.FillWeight = 3F;
+            this.Telephely.HeaderText = "Telephely";
+            this.Telephely.Name = "Telephely";
+            // 
+            // Ferohely
+            // 
+            this.Ferohely.FillWeight = 3F;
+            this.Ferohely.HeaderText = "Férőhely";
+            this.Ferohely.Name = "Ferohely";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvEpuletek);
+            this.Name = "FrmMain";
+            this.Text = "Ceg";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEpuletek)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvEpuletek;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Telephely;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Ferohely;
+    }
+}
+
---
+++ 191202/FrmMain.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace _191202
+{
+    public partial class FrmMain : Form
+    {
+        SqlConnection conn;
+        public FrmMain()
+        {
+            conn = new SqlConnection(@"Server = (localdb)\MSSQLLocalDB;Database = Ceg;");
+
+            InitializeComponent();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            conn.Open();
+
+            var sql = new SqlCommand("SELECT * FROM epuletek",conn);
+            SqlDataReader reader = sql.ExecuteReader();
+
+            while (reader.Read())
+            {
+                
+
+                dgvEpuletek.Rows.Add(reader[0], reader[1], reader[2]);
+            }
+            conn.Close();
+
+
+            
+        }
+    }
+}
